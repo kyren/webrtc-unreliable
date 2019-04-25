da28d19bf73e4301a3b45ae419c297b602ab616d
kyren <kerriganw@gmail.com>
2019-04-25 00:37:45 -0400

    SCTP bugfixes, untested packet writing

--- src/sctp.rs
+++ src/sctp.rs
@@ -1,7 +1,7 @@
-use std::{error::Error, fmt};
+use std::{convert::TryInto, error::Error, fmt};
 
 use byteorder::{ByteOrder, LittleEndian, NetworkEndian};
-use crc32c::crc32c_append;
+use crc32c::{crc32c, crc32c_append};
 
 pub const SCTP_FLAG_END_FRAGMENT: u8 = 0x01;
 pub const SCTP_FLAG_BEGIN_FRAGMENT: u8 = 0x02;
@@ -33,7 +33,7 @@ pub enum SctpChunk<'a> {
         num_outbound_streams: u16,
         num_inbound_streams: u16,
         initial_tsn: u32,
-        cookie: &'a [u8],
+        state_cookie: &'a [u8],
     },
     SAck {
         cumulative_tsn_ack: u32,
@@ -52,7 +52,7 @@ pub enum SctpChunk<'a> {
         cumulative_tsn_ack: u32,
     },
     CookieEcho {
-        cookie: &'a [u8],
+        state_cookie: &'a [u8],
     },
     CookieAck,
     ForwardTsn {
@@ -80,7 +80,7 @@ impl fmt::Display for SctpReadError {
         match self {
             SctpReadError::BadPacket => write!(f, "bad sctp packet"),
             SctpReadError::BadChecksum => write!(f, "bad sctp checksum"),
-            SctpReadError::TooManyChunks => write!(f, "too many sctp chunks"),
+            SctpReadError::TooManyChunks => write!(f, "too many sctp chunks for provided buffer"),
         }
     }
 }
@@ -118,13 +118,13 @@ pub fn read_sctp_packet<'a>(
 
         let chunk_type = remaining_chunks[0];
         let chunk_flags = remaining_chunks[1];
-        let chunk_length = NetworkEndian::read_u16(&remaining_chunks[2..4]);
+        let chunk_len = NetworkEndian::read_u16(&remaining_chunks[2..4]);
 
-        if chunk_length as usize >= remaining_chunks.len() {
+        if chunk_len as usize > remaining_chunks.len() {
             return Err(SctpReadError::BadPacket);
         }
 
-        let chunk_data = &remaining_chunks[4..chunk_length as usize];
+        let chunk_data = &remaining_chunks[4..chunk_len as usize];
         match chunk_type {
             CHUNK_TYPE_DATA => {
                 if chunk_data.len() < 12 {
@@ -173,7 +173,7 @@ pub fn read_sctp_packet<'a>(
                     let param_len = NetworkEndian::read_u16(&chunk_data[18..20]);
 
                     if param_type != INIT_ACK_PARAM_STATE_COOKIE
-                        || 20 + param_len as usize >= chunk_data.len()
+                        || 16 + param_len as usize > chunk_data.len()
                     {
                         return Err(SctpReadError::BadPacket);
                     }
@@ -184,7 +184,7 @@ pub fn read_sctp_packet<'a>(
                         num_outbound_streams,
                         num_inbound_streams,
                         initial_tsn,
-                        cookie: &chunk_data[20..20 + param_len as usize],
+                        state_cookie: &chunk_data[20..16 + param_len as usize],
                     };
                 }
             }
@@ -210,10 +210,9 @@ pub fn read_sctp_packet<'a>(
                 if chunk_data.len() > 4 {
                     let param_type = NetworkEndian::read_u16(&chunk_data[0..2]);
                     let param_len = NetworkEndian::read_u16(&chunk_data[2..4]);
-                    if param_type == HEARTBEAT_PARAM_INFO
-                        && 4 + (param_len as usize) < chunk_data.len()
+                    if param_type == HEARTBEAT_PARAM_INFO && (param_len as usize) < chunk_data.len()
                     {
-                        heartbeat_info = Some(&chunk_data[4..4 + param_len as usize]);
+                        heartbeat_info = Some(&chunk_data[4..param_len as usize]);
                     }
                 }
 
@@ -235,7 +234,11 @@ pub fn read_sctp_packet<'a>(
 
                 *chunk = SctpChunk::Shutdown { cumulative_tsn_ack };
             }
-            CHUNK_TYPE_COOKIE_ECHO => *chunk = SctpChunk::CookieEcho { cookie: chunk_data },
+            CHUNK_TYPE_COOKIE_ECHO => {
+                *chunk = SctpChunk::CookieEcho {
+                    state_cookie: chunk_data,
+                }
+            }
             CHUNK_TYPE_COOKIE_ACK => {
                 *chunk = SctpChunk::CookieAck;
             }
@@ -250,7 +253,7 @@ pub fn read_sctp_packet<'a>(
             _ => unimplemented!(),
         }
 
-        remaining_chunks = &remaining_chunks[chunk_length as usize..];
+        remaining_chunks = &remaining_chunks[chunk_len as usize..];
         chunk_count += 1;
     }
 
@@ -262,8 +265,224 @@ pub fn read_sctp_packet<'a>(
     })
 }
 
-pub fn write_sctp_packet(dest: &mut [u8], packet: SctpPacket) -> Option<usize> {
-    unimplemented!()
+#[derive(Debug)]
+pub enum SctpWriteError {
+    BufferSize,
+    NoChunks,
+    OutOfRange,
+}
+
+impl fmt::Display for SctpWriteError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self {
+            SctpWriteError::BufferSize => write!(f, "insufficient output buffer size"),
+            SctpWriteError::NoChunks => write!(f, "SCTP packet must have at least one chunk"),
+            SctpWriteError::OutOfRange => write!(f, "SCTP packet field is too large"),
+        }
+    }
+}
+
+impl Error for SctpWriteError {}
+
+pub fn write_sctp_packet(dest: &mut [u8], packet: SctpPacket) -> Result<usize, SctpWriteError> {
+    if packet.chunks.is_empty() {
+        return Err(SctpWriteError::NoChunks);
+    }
+
+    if dest.len() < 12 {
+        return Err(SctpWriteError::BufferSize);
+    }
+
+    NetworkEndian::write_u16(&mut dest[0..2], packet.source_port);
+    NetworkEndian::write_u16(&mut dest[2..4], packet.dest_port);
+    NetworkEndian::write_u32(&mut dest[4..8], packet.verification_tag);
+    dest[8..12].copy_from_slice(&[0, 0, 0, 0]);
+
+    let mut rest = &mut dest[12..];
+
+    for &chunk in packet.chunks {
+        if rest.len() < 8 {
+            return Err(SctpWriteError::BufferSize);
+        }
+
+        let (chunk_header, chunk_data) = rest.split_at_mut(8);
+        let (chunk_type, chunk_flags, data_len) = match chunk {
+            SctpChunk::Data {
+                chunk_flags,
+                tsn,
+                stream_id,
+                stream_seq,
+                proto_id,
+                user_data,
+            } => {
+                let data_len = 12 + user_data.len();
+                if chunk_data.len() < data_len {
+                    return Err(SctpWriteError::BufferSize);
+                }
+
+                NetworkEndian::write_u32(&mut chunk_data[0..4], tsn);
+                NetworkEndian::write_u16(&mut chunk_data[4..6], stream_id);
+                NetworkEndian::write_u16(&mut chunk_data[6..8], stream_seq);
+                NetworkEndian::write_u32(&mut chunk_data[8..12], proto_id);
+                chunk_data[12..].copy_from_slice(user_data);
+
+                (CHUNK_TYPE_DATA, chunk_flags, data_len)
+            }
+            SctpChunk::Init {
+                initiate_tag,
+                window_credit,
+                num_outbound_streams,
+                num_inbound_streams,
+                initial_tsn,
+            } => {
+                let data_len = 16;
+                if chunk_data.len() < data_len {
+                    return Err(SctpWriteError::BufferSize);
+                }
+
+                NetworkEndian::write_u32(&mut chunk_data[0..4], initiate_tag);
+                NetworkEndian::write_u32(&mut chunk_data[4..8], window_credit);
+                NetworkEndian::write_u16(&mut chunk_data[8..10], num_outbound_streams);
+                NetworkEndian::write_u16(&mut chunk_data[10..12], num_inbound_streams);
+                NetworkEndian::write_u32(&mut chunk_data[12..16], initial_tsn);
+
+                (CHUNK_TYPE_INIT, 0, data_len)
+            }
+            SctpChunk::InitAck {
+                initiate_tag,
+                window_credit,
+                num_outbound_streams,
+                num_inbound_streams,
+                initial_tsn,
+                state_cookie,
+            } => {
+                let data_len = 20 + state_cookie.len();
+                if chunk_data.len() < data_len {
+                    return Err(SctpWriteError::BufferSize);
+                }
+
+                NetworkEndian::write_u32(&mut chunk_data[0..4], initiate_tag);
+                NetworkEndian::write_u32(&mut chunk_data[4..8], window_credit);
+                NetworkEndian::write_u16(&mut chunk_data[8..10], num_outbound_streams);
+                NetworkEndian::write_u16(&mut chunk_data[10..12], num_inbound_streams);
+                NetworkEndian::write_u32(&mut chunk_data[12..16], initial_tsn);
+                NetworkEndian::write_u16(&mut chunk_data[16..18], INIT_ACK_PARAM_STATE_COOKIE);
+                NetworkEndian::write_u16(
+                    &mut chunk_data[18..20],
+                    state_cookie
+                        .len()
+                        .try_into()
+                        .map_err(|_| SctpWriteError::OutOfRange)?,
+                );
+
+                chunk_data[20..].copy_from_slice(state_cookie);
+
+                (CHUNK_TYPE_INIT_ACK, 0, data_len)
+            }
+            SctpChunk::SAck {
+                cumulative_tsn_ack,
+                adv_recv_window,
+                num_gap_ack_blocks,
+                num_dup_tsn,
+            } => {
+                let data_len = 12;
+                if chunk_data.len() < data_len {
+                    return Err(SctpWriteError::BufferSize);
+                }
+
+                NetworkEndian::write_u32(&mut chunk_data[0..4], cumulative_tsn_ack);
+                NetworkEndian::write_u32(&mut chunk_data[4..8], adv_recv_window);
+                NetworkEndian::write_u16(&mut chunk_data[8..10], num_gap_ack_blocks);
+                NetworkEndian::write_u16(&mut chunk_data[10..12], num_dup_tsn);
+
+                (CHUNK_TYPE_SACK, 0, data_len)
+            }
+            SctpChunk::Heartbeat { heartbeat_info }
+            | SctpChunk::HeartbeatAck { heartbeat_info } => {
+                let chunk_type = if let SctpChunk::Heartbeat { .. } = chunk {
+                    CHUNK_TYPE_HEARTBEAT
+                } else {
+                    CHUNK_TYPE_HEARTBEAT_ACK
+                };
+
+                let data_len = if let Some(heartbeat_info) = heartbeat_info {
+                    let data_len = 4 + heartbeat_info.len();
+                    if chunk_data.len() < data_len {
+                        return Err(SctpWriteError::BufferSize);
+                    }
+
+                    NetworkEndian::write_u16(&mut chunk_data[0..2], HEARTBEAT_PARAM_INFO);
+                    NetworkEndian::write_u16(
+                        &mut chunk_data[2..4],
+                        heartbeat_info
+                            .len()
+                            .try_into()
+                            .map_err(|_| SctpWriteError::OutOfRange)?,
+                    );
+                    chunk_data[4..].copy_from_slice(heartbeat_info);
+                    data_len
+                } else {
+                    0
+                };
+
+                (chunk_type, 0, data_len)
+            }
+            SctpChunk::Abort => (CHUNK_TYPE_ABORT, 0, 0),
+            SctpChunk::Shutdown { cumulative_tsn_ack } => {
+                let data_len = 4;
+                if chunk_data.len() < 4 {
+                    return Err(SctpWriteError::BufferSize);
+                }
+                NetworkEndian::write_u32(&mut chunk_data[0..4], cumulative_tsn_ack);
+                (CHUNK_TYPE_SHUTDOWN, 0, data_len)
+            }
+            SctpChunk::CookieEcho { state_cookie } => {
+                if chunk_data.len() < state_cookie.len() {
+                    return Err(SctpWriteError::BufferSize);
+                }
+                chunk_data.copy_from_slice(state_cookie);
+                (CHUNK_TYPE_COOKIE_ECHO, 0, state_cookie.len())
+            }
+            SctpChunk::CookieAck => (CHUNK_TYPE_COOKIE_ACK, 0, 0),
+            SctpChunk::ForwardTsn { new_cumulative_tsn } => {
+                let data_len = 4;
+                if chunk_data.len() < 4 {
+                    return Err(SctpWriteError::BufferSize);
+                }
+                NetworkEndian::write_u32(&mut chunk_data[0..4], new_cumulative_tsn);
+                (CHUNK_TYPE_FORWARD_TSN, 0, data_len)
+            }
+        };
+
+        let data_len = if data_len % 4 == 0 {
+            data_len
+        } else {
+            let len = data_len - data_len % 4 + 4;
+            for i in data_len..len {
+                chunk_data[i] = 0;
+            }
+            len
+        };
+        let chunk_len = data_len + 4;
+
+        chunk_header[0] = chunk_type;
+        chunk_header[1] = chunk_flags;
+        NetworkEndian::write_u16(
+            &mut chunk_header[2..4],
+            chunk_len
+                .try_into()
+                .map_err(|_| SctpWriteError::OutOfRange)?,
+        );
+
+        rest = &mut rest[chunk_len..];
+    }
+
+    let remainder = rest.len();
+    let len = dest.len() - remainder;
+    let mut crc = crc32c(&dest[0..len]);
+    LittleEndian::write_u32(&mut dest[8..12], crc);
+
+    Ok(len)
 }
 
 const CHUNK_TYPE_DATA: u8 = 0x00;
