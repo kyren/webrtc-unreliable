45534ee10e2ef828c2356b9fc2e3ac405773223d
kyren <kyren@kyju.org>
2024-04-07 16:22:59 -0600

    Update example to work with hyper 1.0

--- examples/echo-server.rs
+++ examples/echo-server.rs
@@ -1,10 +1,16 @@
+use std::net::SocketAddr;
+
 use clap::{Arg, Command};
+use futures::stream::TryStreamExt;
+use http_body_util::BodyStream;
 use hyper::{
     header::{self, HeaderValue},
-    server::{conn::AddrStream, Server},
-    service::{make_service_fn, service_fn},
-    Body, Error, Method, Response, StatusCode,
+    server::conn::http1,
+    service::service_fn,
+    Method, Response, StatusCode,
 };
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
@@ -37,19 +43,19 @@ async fn main() {
         )
         .get_matches();
 
-    let webrtc_listen_addr = matches
+    let webrtc_listen_addr: SocketAddr = matches
         .value_of("data")
         .unwrap()
         .parse()
         .expect("could not parse WebRTC data address/port");
 
-    let public_webrtc_addr = matches
+    let public_webrtc_addr: SocketAddr = matches
         .value_of("public")
         .unwrap()
         .parse()
         .expect("could not parse advertised public WebRTC data address/port");
 
-    let session_listen_addr = matches
+    let session_listen_addr: SocketAddr = matches
         .value_of("http")
         .unwrap()
         .parse()
@@ -60,51 +66,74 @@ async fn main() {
             .expect("could not start RTC server");
 
     let session_endpoint = rtc_server.session_endpoint();
-    let make_svc = make_service_fn(move |addr_stream: &AddrStream| {
-        let session_endpoint = session_endpoint.clone();
-        let remote_addr = addr_stream.remote_addr();
-        async move {
-            Ok::<_, Error>(service_fn(move |req| {
-                let mut session_endpoint = session_endpoint.clone();
-                async move {
-                    if req.uri().path() == "/"
-                        || req.uri().path() == "/index.html" && req.method() == Method::GET
-                    {
-                        log::info!("serving example index HTML to {}", remote_addr);
-                        Response::builder().body(Body::from(include_str!("./echo-server.html")))
-                    } else if req.uri().path() == "/new_rtc_session" && req.method() == Method::POST
-                    {
-                        log::info!("WebRTC session request from {}", remote_addr);
-                        match session_endpoint.http_session_request(req.into_body()).await {
-                            Ok(mut resp) => {
-                                resp.headers_mut().insert(
-                                    header::ACCESS_CONTROL_ALLOW_ORIGIN,
-                                    HeaderValue::from_static("*"),
-                                );
-                                Ok(resp.map(Body::from))
-                            }
-                            Err(err) => {
-                                log::warn!("bad rtc session request: {:?}", err);
-                                Response::builder()
-                                    .status(StatusCode::BAD_REQUEST)
-                                    .body(Body::from(format!("error: {:?}", err)))
-                            }
-                        }
-                    } else {
-                        Response::builder()
-                            .status(StatusCode::NOT_FOUND)
-                            .body(Body::from("not found"))
-                    }
-                }
-            }))
-        }
-    });
 
     tokio::spawn(async move {
-        Server::bind(&session_listen_addr)
-            .serve(make_svc)
+        let listener = TcpListener::bind(session_listen_addr)
             .await
-            .expect("HTTP session server has died");
+            .expect("could not listen on HTTP address/port");
+
+        loop {
+            match listener.accept().await {
+                Err(err) => {
+                    log::warn!("error accepting incoming HTTP connection: {:?}", err);
+                }
+                Ok((stream, remote_addr)) => {
+                    let io = TokioIo::new(stream);
+                    let session_endpoint = session_endpoint.clone();
+                    tokio::spawn(async move {
+                        if let Err(err) = http1::Builder::new()
+                            .serve_connection(
+                                io,
+                                service_fn(|req| async {
+                                    let mut session_endpoint = session_endpoint.clone();
+                                    if req.uri().path() == "/"
+                                        || req.uri().path() == "/index.html"
+                                            && req.method() == Method::GET
+                                    {
+                                        log::info!("serving example index HTML to {}", remote_addr);
+                                        Response::builder()
+                                            .body(include_str!("./echo-server.html").to_owned())
+                                    } else if req.uri().path() == "/new_rtc_session"
+                                        && req.method() == Method::POST
+                                    {
+                                        log::info!("WebRTC session request from {}", remote_addr);
+                                        match session_endpoint
+                                            .http_session_request(
+                                                BodyStream::new(req.into_body()).try_filter_map(
+                                                    |f| async { Ok(f.into_data().ok()) },
+                                                ),
+                                            )
+                                            .await
+                                        {
+                                            Ok(mut resp) => {
+                                                resp.headers_mut().insert(
+                                                    header::ACCESS_CONTROL_ALLOW_ORIGIN,
+                                                    HeaderValue::from_static("*"),
+                                                );
+                                                Ok(resp)
+                                            }
+                                            Err(err) => {
+                                                log::warn!("bad rtc session request: {:?}", err);
+                                                Response::builder()
+                                                    .status(StatusCode::BAD_REQUEST)
+                                                    .body(format!("error: {:?}", err))
+                                            }
+                                        }
+                                    } else {
+                                        Response::builder()
+                                            .status(StatusCode::NOT_FOUND)
+                                            .body("not found".to_owned())
+                                    }
+                                }),
+                            )
+                            .await
+                        {
+                            log::warn!("error serving connection: {:?}", err);
+                        }
+                    });
+                }
+            }
+        }
     });
 
     let mut message_buf = Vec::new();
